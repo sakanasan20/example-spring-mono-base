0ee954ceaccd98fa79f831c79da4b3d297ef61af
Nick <nick.chen@cmp.com.tw>
2023-06-16 10:50:14 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringMonoBaseApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringMonoBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringMonoBaseApplication.class, args);
+	}
+
+}
