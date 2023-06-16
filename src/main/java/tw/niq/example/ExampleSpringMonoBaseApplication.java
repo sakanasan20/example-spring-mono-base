89b1b3e771af46ef91aefb2a2c57022b4de5255b
Nick <nick.chen@cmp.com.tw>
2023-06-16 14:11:42 +0800

    Add encoder, mapper bean

--- src/main/java/tw/niq/example/ExampleSpringMonoBaseApplication.java
+++ src/main/java/tw/niq/example/ExampleSpringMonoBaseApplication.java
@@ -1,13 +1,29 @@
 package tw.niq.example;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ExampleSpringMonoBaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleSpringMonoBaseApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
 	}
 
 }
