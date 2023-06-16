d8b16b5dc489ae763b6aefcc3c01327f256ef77b
Nick <nick.chen@cmp.com.tw>
2023-06-16 14:12:13 +0800

    Update user entity properties

--- src/main/java/tw/niq/example/entity/UserEntity.java
+++ src/main/java/tw/niq/example/entity/UserEntity.java
@@ -1,11 +1,17 @@
 package tw.niq.example.entity;
 
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Builder;
 import lombok.Data;
 
@@ -24,5 +30,21 @@ public class UserEntity {
 	
 	@Column(nullable = false, unique = true)
 	private String password;
+	
+	@Builder.Default
+	private Boolean enabled = true;
+	
+	@Builder.Default
+	private Boolean accountNonExpired = true;
+	
+	@Builder.Default
+	private Boolean credentialsNonExpired = true;
+	
+	@Builder.Default
+	private Boolean accountNonLocked = true;
+	
+	@Transient
+	@Builder.Default
+	Collection<? extends GrantedAuthority> authorities = new HashSet<>();
 	
 }
